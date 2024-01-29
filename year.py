4bf2eac7c88adad0c69087ab9f7df1545b3de4ed
Bitan09 <155796631+Bitan09@users.noreply.github.com>
2024-01-29 20:32:58 +0530

    date.py

--- year.py
+++ year.py
@@ -6,26 +6,6 @@ months={1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
 if m>12 or m<1 or d<1 or d>months[m]:
     print('error')
 else:
-    '''s=0
-    s+=(months[m]-d)
-    for i in range(m+1,13):
-        s+=months[i]
-    while days>=365:
-        if (y%4==0 and y%100!=0) or y==400:
-            yn=366
-        else:
-            yn=365
-        days-=yn
-        y+=1'''
-    '''if days>=s:
-        y+=1
-        if (y%4==0 and y%100!=0) or y==400:
-            months.update({2:29})
-        else:
-            months.update({2:28})
-        days-=s
-        d=1
-        m=1'''
     if days!=0:
         while days>=(months[m]-d):
             if (y%4==0 and y%100!=0) or y==400:
